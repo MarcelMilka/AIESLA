6b683b686b564e8ee9a0a24e227415c64fa50545
Marcel Milka <m.milka.1996@wp.pl>
2025-02-24 20:12:25 +0100

    - Implement test class.

---
+++ app/src/androidTest/java/eu/project/aiesla/uiTests/TestOfNavigation.kt
@@ -0,0 +1,9 @@
+package eu.project.aiesla.uiTests
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class TestOfNavigation {
+
+    @get:Rule val ctr = createComposeRule()
+}
