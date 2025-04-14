aaaf59599d37513cc16dc0aeafdeb29c75afc622
Marcel Milka <marcel.milka.2006@gmail.com>
2025-04-14 20:56:20 +0200

    - Implemented :sharedUi and some basic things.

---
+++ sharedUi/src/main/java/com/example/sharedui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.sharedui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFFFE752F)
+val Background = Color(0xFF131313)
+
+val PrimaryWhite = Color(0xFFFFFFFF)
+val SecondaryWhite = Color(0x80FFFFFF)
